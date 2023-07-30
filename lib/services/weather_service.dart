e1ca5ce01de620d745a23d538cd9d03d3da36160
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-07-30 10:17:52 +0300

    Searching_city

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,12 +1,19 @@
 // ignore_for_file: unused_local_variable, non_constant_identifier_names
 
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class WeatherService {
+  String BaseURL = 'http://api.weatherapi.com/v1';
+  String APIKey = 'ca261ea9d4f14a78a9285745232807';
+
   void getWeather({required String CityName}) async {
-    Uri url = Uri.parse(
-        'http://api.weatherapi.com/v1/current.json?key=ca261ea9d4f14a78a9285745232807&q=London&aqi=no');
+    Uri url =
+        Uri.parse('$BaseURL/forecast.json?key=$APIKey&q=$CityName&days=7');
 
     http.Response response = await http.get(url);
+
+    Map<dynamic, dynamic> JsonData = jsonDecode(response.body);
+    print(JsonData);
   }
 }
