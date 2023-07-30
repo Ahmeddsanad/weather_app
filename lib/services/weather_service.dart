25970bf721a1f6a27d35cbfce28440b99c544526
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-07-30 10:51:20 +0300

    weather_model

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -3,27 +3,43 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:weather_app/models/weather_model.dart';
+
 class WeatherService {
   String BaseURL = 'http://api.weatherapi.com/v1';
   String APIKey = 'ca261ea9d4f14a78a9285745232807';
 
-  void getWeather({required String CityName}) async {
+  Future<WeatherModel> getWeather({required String CityName}) async {
     Uri url =
         Uri.parse('$BaseURL/forecast.json?key=$APIKey&q=$CityName&days=7');
 
     http.Response response = await http.get(url);
 
     Map<dynamic, dynamic> JsonData = jsonDecode(response.body);
-    // print(JsonData);
 
-    String date = JsonData['location']['localtime'];
+    var ListNumOne = JsonData['forecast']['forecastday'][0]['day'];
 
-    String Icon = JsonData['current']['condition']
-        ['https://cdn.weatherapi.com/weather/64x64/day/122.png'];
+    WeatherModel weather = WeatherModel(
+      date: JsonData['location']['localtime'],
+      WeatherStateName: ListNumOne['condition']['text'],
+      maxTemp: ListNumOne['maxtemp_c'],
+      minTemp: ListNumOne['mintemp_c'],
+      temp: ListNumOne['avgtemp_c'],
+    );
+
+    return weather;
+  }
+}
+
+
+// print(JsonData);
+
+    // String date = JsonData['location']['localtime'];
+
+    // String Icon = JsonData['current']['condition']
+        // ['https://cdn.weatherapi.com/weather/64x64/day/122.png'];
 
     // when we accessing a list '[ ]' We use [0] [1] ... [inf.]
     // when we accessing a map ['String from Json']
 
-    String temp = JsonData['forecast']['forecastday'][0]['day']['avgtemp_c'];
-  }
-}
+    // String temp = JsonData['forecast']['forecastday'][0]['day']['avgtemp_c'];
